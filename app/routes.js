f81f0eb3808a49b33570a6b6fdd777acd1dc58b8
Brian Coult <ethay@icloud.com>
2024-07-08 12:29:45 +0100

    removed routing

--- app/routes.js
+++ app/routes.js
@@ -14,12 +14,6 @@ router.use(radioButtonRedirect)
 // Add your routes here
 
 
-// The URL here needs to match the URL of the page that the user is on
-// when they type in their email address
+  
 
   
-    // This is the URL the users will be redirected to once the email
-    // has been sent
-    res.redirect('/find/confirmation');
-  
-  });
