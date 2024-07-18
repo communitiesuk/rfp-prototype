4852e7347b14cdb853c12a96c4ef325d827b4364
Brian Coult <ethay@icloud.com>
2024-07-18 08:22:32 +0100

    added small logic

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -3,13 +3,13 @@ module.exports = {
   // Insert values here
 
   projectName: "Growing Community Green Spaces",
-  reportPeriod: "July 2023 to September 2023",
+  reportPeriod: "January 2024 to June 2024",
   projectID: "123456",  
   completionDate: "30 May 2025",
   contactPerson: "Gemma Currie",
   section151: "Kate Harries",
   fundName: "Pathfinders",
-  fundDate: "2 August 2024",
+  fundDate: "29 July 2024",
   organisationName: "Acme Authority",
   reporterName: "Brian Coult",
   section151Name: "Gemma Currie",
