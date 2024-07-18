fd6a615de8d65b14645162d140f18fff3536663b
Brian Coult <ethay@icloud.com>
2024-07-18 08:55:23 +0100

    back links used for moving navigation

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -6,13 +6,13 @@ module.exports = {
   reportPeriod: "January 2024 to June 2024",
   projectID: "123456",  
   completionDate: "30 May 2025",
-  contactPerson: "Gemma Currie",
+  contactPerson: "Hugo Furst",
   section151: "Kate Harries",
-  fundName: "Pathfinders",
+  fundName: "Towns Fund",
   fundDate: "29 July 2024",
-  organisationName: "Acme Authority",
-  reporterName: "Brian Coult",
-  section151Name: "Gemma Currie",
+  organisationName: "Hastings Council",
+  reporterName: "Ruth Kynoch",
+  section151Name: "Hugo Furst",
 
   
 }
