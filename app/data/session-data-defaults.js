d9b9b246a59a24027d78435814cd8b13bc8ae86e
Brian Coult <brian.coult@levellingup.gov.uk>
2024-07-29 15:20:36 +0100

    ur default

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -11,9 +11,9 @@ module.exports = {
   fundName: "Pathfinders",
   fundDate: "8 August 2024",
   dueDate: "29 July 2024",
-  organisationName: "Sheffield Council",
-  reporterName: "Jo Calcutt-Scott",
-  reporterEmail: "jo.calcutt-scott@user-research.gov.uk",
+  organisationName: "ACME Council",
+  reporterName: "Jordan Dale",
+  reporterEmail: "jordan.dale@user-research.gov.uk",
   section151Name: "Hugo Furst",
   section151Email: "Hugo.Furst@user-research.gov.uk"
 }
