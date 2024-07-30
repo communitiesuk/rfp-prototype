c70be0758afd88a86e9830491e58286f67bf380c
Brian Coult <brian.coult@levellingup.gov.uk>
2024-07-30 09:28:10 +0100

    new ur participant

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -8,12 +8,12 @@ module.exports = {
   completionDate: "30 May 2025",
   contactPerson: "Hugo Furst",
   section151: "Kate Harries",
-  fundName: "Pathfinders",
-  fundDate: "8 August 2024",
-  dueDate: "29 July 2024",
-  organisationName: "ACME Council",
-  reporterName: "Jordan Dale",
-  reporterEmail: "jordan.dale@user-research.gov.uk",
+  fundName: "Towns Fund",
+  fundDate: "9 August 2024",
+  dueDate: "30 July 2024",
+  organisationName: "Stockport Metropolitan Council",
+  reporterName: "Louis Manifould",
+  reporterEmail: "louis.manifould@user-research.gov.uk",
   section151Name: "Hugo Furst",
   section151Email: "Hugo.Furst@user-research.gov.uk"
 }
